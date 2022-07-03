13f854663620396951db9306c1d3f2dab69bc168
WoojinKim <dnwls488@naver.com>
2022-07-03 20:03:04 +0900

    threadlocal 을 통한 동시성 문제 해결

---
+++ src/test/java/hello/advanced/trace/threadlocal/code/ThreadLocalServiceTest.java
@@ -0,0 +1,44 @@
+package hello.advanced.trace.threadlocal.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            service.logic("userA");
+        };
+        Runnable userB = () -> {
+            service.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000);    // 동시성 문제 발생X
+        sleep(100); // 동시성 문제 발생O
+        threadB.start();
+
+        sleep(3000); // 메인 thread 종료 대기
+        log.info("main exit");
+
+//        count down 렛지
+    }
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

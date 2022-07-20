82a7c378a93b2a5d72ea2e513eaff2a8b23590ac
WoojinKim <dnwls488@naver.com>
2022-07-20 19:38:30 +0900

    추상화를 통한 공통 처리 - Reflection 의 경우 Runtime 시 동작하기 때문에 컴파일 시점에 오류를 잡을 수 없음

--- src/test/java/hello/advanced/jdkdynamic/ReflectionTest.java
+++ src/test/java/hello/advanced/jdkdynamic/ReflectionTest.java
@@ -3,6 +3,10 @@ package hello.advanced.jdkdynamic;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.Target;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 @Slf4j
 public class ReflectionTest {
 
@@ -10,8 +14,8 @@ public class ReflectionTest {
     void reflection0() {
         Hello target = new Hello();
 
-
         /**
+         * 로직1 <> 로직2
          * 호출하는 메서드만 다름
          */
 
@@ -26,7 +30,46 @@ public class ReflectionTest {
         String result2 = target.callB();
         log.info("result={}", result2);
         //공통 로직2 종료
+    }
 
+    @Test
+    void reflection1() throws Exception {
+        //클래스 메타정보
+        Class classHello = Class.forName("hello.advanced.jdkdynamic.ReflectionTest$Hello");
+
+        Hello target = new Hello();
+        //callA 메서드 정보
+        Method methodCallA = classHello.getMethod("callA");
+        Object result1 = methodCallA.invoke(target);
+        log.info("result={}", result1);
+
+        //callB 메서드 정보
+        Method methodCallB = classHello.getMethod("callB");
+        Object result2 = methodCallB.invoke(target);
+        log.info("result={}", result2);
+    }
+
+    @Test
+    void reflection2() throws Exception {
+        Class classHello = Class.forName("hello.advanced.jdkdynamic.ReflectionTest$Hello");
+
+        /**
+         * 추상화를 통한 공통 처리
+         * - Reflection 의 경우 Runtime 시 동작하기 때문에
+         * 컴파일 시점에 오류를 잡을 수 없음
+         */
+        Hello target = new Hello();
+        Method methodCallA = classHello.getMethod("callA");
+        dynamicCall(methodCallA, target);
+
+        Method methodCallB = classHello.getMethod("callB");
+        dynamicCall(methodCallB, target);
+    }
+
+    private void dynamicCall(Method method, Object target) throws Exception {
+        log.info("start");
+        Object result = method.invoke(target);
+        log.info("result={}", result);
     }
 
     @Slf4j

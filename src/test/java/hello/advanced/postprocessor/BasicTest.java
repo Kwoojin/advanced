e7a53e5d8ae78fb0e26450d7da6391fde0cdd485
WoojinKim <dnwls488@naver.com>
2022-07-24 22:44:19 +0900

    빈 후처리기 init - bean 등록

--- src/test/java/hello/advanced/postprocessor/BasicTest.java
+++ src/test/java/hello/advanced/postprocessor/BasicTest.java
@@ -1,11 +1,15 @@
 package hello.advanced.postprocessor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class BasicTest {
 
@@ -17,6 +21,9 @@ public class BasicTest {
         A a = applicationContext.getBean("beanA", A.class);
         a.helloA();
 
+        //B는 빈으로 등록되지 않는다. ( 후 처리기용 )
+        assertThatThrownBy(() -> applicationContext.getBean(B.class))
+                .isInstanceOf(NoSuchBeanDefinitionException.class);
     }
 
     @Slf4j
